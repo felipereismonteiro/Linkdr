9c3b10d2f651a065a04bc958a3f2e7391d60004f
Rayandson <rayandson.silva321@gmail.com>
2023-01-03 21:44:53 -0300

    Feat: Timeline Page

---
+++ src/components/Title.js
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+export default function Title() {
+    return(
+        <Header>timeline</Header>
+    )
+}
+
+const Header = styled.h1`
+    font-family: 'Oswald';
+    font-weight: 700;
+    font-size: 43px;
+    color: #FFFFFF;
+    font-family: 'Oswald', sans-serif;
+    margin-bottom: 43px;
+`
