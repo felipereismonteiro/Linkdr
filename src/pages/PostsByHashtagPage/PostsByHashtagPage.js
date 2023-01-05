f53c1baee66ea0101f91c067443b1e4fa5edbea8
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2023-01-05 11:59:06 -0300

    feat: add posts fetch by hashtag id

--- src/pages/PostsByHashtagPage/PostsByHashtagPage.js
+++ src/pages/PostsByHashtagPage/PostsByHashtagPage.js
@@ -5,24 +5,40 @@ import { useLocation, useParams } from "react-router-dom";
 import Navbar from "../../components/Navbar";
 import Post from "../../components/Post";
 import HashtagTable from "../../components/HashtagTable/HashtagTable";
+import { useEffect, useState } from "react";
+import api from "../../services/api";
 
 export default function PostsByHashtagPage() {
+  const [posts, setPosts] = useState(null);
   const { hashtag } = useParams();
-  const location = useLocation();
+  const { state } = useLocation();
 
-  const post = {
-    profile_picture: "teste",
-    user_name: "teste",
-    content: "teste",
-    url: "teste",
-  };
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const resp = await api.getPostsByHashtagId(state);
+        setPosts(resp.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  if (!posts) {
+    return <div>Loading</div>;
+  }
+
   return (
     <>
       <Navbar />
       <PageContainer>
         <MainContent>
           <Title title={hashtag} />
-          <Post post={post} />
+          {posts.map((post) => (
+            <Post key={post.id} post={post} />
+          ))}
         </MainContent>
         <HashtagTable />
       </PageContainer>
