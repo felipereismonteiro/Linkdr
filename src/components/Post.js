bc4d83112ec8bd5d37e6412667a101169474fda0
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2023-01-05 17:07:48 -0300

    feat: add targeting the hashtag/:hashtag route from the post tag

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,65 +1,83 @@
 import styled from "styled-components";
+import { ReactTagify } from "react-tagify";
+import { useNavigate } from "react-router-dom";
 
-export default function Post({post}) {
+export default function Post({ post }) {
+  const navigate = useNavigate();
+  const tagStyle = {
+    color: "white",
+    fontWeight: "bold",
+    cursor: "pointer",
+  };
 
-    return(
-        <Container>
-             <UserPic src={post.profile_picture} alt="User picture"/>
-                <PostContent>
-                    <Username>{post.user_name}</Username>
-                    <Description>{post.content}</Description>
-                    <PostUrl>{post.url}</PostUrl>
-                </PostContent>
-        </Container>
-    )
+  function goToPostsByHashtagPage(tag) {
+    const formattedTag = tag.replace("#", "");
+    navigate(`/hashtag/${formattedTag}`);
+  }
+
+  return (
+    <Container>
+      <UserPic src={post.profile_picture} alt="User picture" />
+      <PostContent>
+        <Username>{post.user_name}</Username>
+        <ReactTagify
+          tagStyle={tagStyle}
+          tagClicked={(tag) => goToPostsByHashtagPage(tag)}
+        >
+          <Description>{post.content}</Description>
+        </ReactTagify>
+
+        <PostUrl>{post.url}</PostUrl>
+      </PostContent>
+    </Container>
+  );
 }
 
 const Container = styled.div`
-    width: 100%;
-    height: 276px;
-    background: #171717;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 16px;
-    display: flex;
-    justify-content: center;
-    align-items: flex-start;
-    gap: 16px;
-    padding: 16px 18px;
-    margin-bottom: 16px;
-`
+  width: 100%;
+  height: 276px;
+  background: #171717;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 16px;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 16px;
+  padding: 16px 18px;
+  margin-bottom: 16px;
+`;
 
 const UserPic = styled.img`
-    width: 53px;
-    height: 53px;
-    border-radius: 26.5px;
-    
-`
+  width: 53px;
+  height: 53px;
+  border-radius: 26.5px;
+`;
 const PostContent = styled.div`
-    width: 503px;
-    height: 170px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    /* background-color: black; */
-    margin-top: 5px;
-`
+  width: 503px;
+  height: 170px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  /* background-color: black; */
+  margin-top: 5px;
+`;
 const Username = styled.h2`
-    font-family: 'Lato';
-    font-weight: 400;
-    font-size: 19px;
-    color: #FFFFFF;
-    margin-bottom: 10px;
-`
+  font-family: "Lato";
+  font-weight: 400;
+  font-size: 19px;
+  color: #ffffff;
+  margin-bottom: 10px;
+`;
 const Description = styled.p`
-    font-family: 'Lato';
-    font-weight: 400;
-    font-size: 17px;
-    color: #B7B7B7;
-`
+  font-family: "Lato";
+  font-weight: 400;
+  font-size: 17px;
+  color: #b7b7b7;
+`;
 
 const PostUrl = styled.p`
-    font-family: 'Lato';
-    font-weight: 400;
-    font-size: 17px;
-    color: #B7B7B7;
-`
+  font-family: "Lato";
+  font-weight: 400;
+  font-size: 17px;
+  color: #b7b7b7;
+`;
