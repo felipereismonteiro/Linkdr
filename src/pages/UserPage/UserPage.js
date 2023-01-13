865f0e54b7232ee427fb0088ba2033590a1f430a
João Vitor Feliciano <110498176+jvitorfeliciano@users.noreply.github.com>
2023-01-13 13:25:18 -0300

    Revert "Revert "Follow""

--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -6,21 +6,26 @@ import { useParams, useNavigate } from "react-router-dom";
 import Navbar from "../../components/NavBar/Navbar";
 import Post from "../../components/Post/Post";
 import SearchBarComponent from "../../components/NavBar/SearchBarComponent.js";
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState, useContext, useRef } from "react";
 import api from "../../services/api";
 import styled from "styled-components";
 import { TokenContext } from "../../contexts/TokenContext.js";
 import FollowStatusButton from "../../components/FollowStatusButton/FollowStatusButton";
 import { UserContext } from "../../contexts/UserContext";
+import InfiniteScroll from "react-infinite-scroller";
+import { ScrollLoading } from "../../components/ScrollLoading/ScrollLoading";
 
 export default function UserPage() {
-  const [data, setData] = useState(null);
-  const [update, setUpdate]=useState(false);
+  const [personalData, setPersonalData] = useState(null);
+  const [update, setUpdate] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+  const [posts, setPosts] = useState([]);
+  const [followStatus, setFollowStatus] = useState(null);
   const { token } = useContext(TokenContext);
   const { user } = useContext(UserContext);
+  const initialPage = useRef(1);
   const userData = JSON.parse(localStorage.getItem("userData"));
   const navigate = useNavigate();
-  console.log(user);
   const { id } = useParams();
 
   useEffect(() => {
@@ -31,16 +36,22 @@ export default function UserPage() {
 
   useEffect(() => {
     if (token) {
-      renderPosts();
+      renderPosts(initialPage.current);
     }
-  }, [id, token, update]);
+  }, [id, token]);
 
-  const renderPosts = async () => {
- 
+  const renderPosts = async (page) => {
+    console.log(page);
     try {
-      const result = await api.getPostsByUserId(id, token);
-      setData(result.data);
-      console.log(result.data)
+      const result = await api.getPostsByUserId(id, page, token);
+
+      setPersonalData(result.data.userInfo);
+      setFollowStatus(result.data.is_followed);
+      setPosts([...posts, ...result.data.posts]);
+      console.log(result.data.posts.length, result.data.posts);
+      if (result.data.posts.length < 10) {
+        setHasMore(false);
+      }
     } catch (err) {
       console.log(err.message);
     }
@@ -57,21 +68,37 @@ export default function UserPage() {
         <SearchBarContainer>
           <SearchBarComponent />
         </SearchBarContainer>
-        {data === null ? (
+        {personalData === null ? (
           <Loading>Loading...</Loading>
         ) : (
           <>
             <MainContent>
               <TitleContainer>
-                <img src={data.userInfo.profile_picture} alt="profile" />
-                <Title title={`${data.userInfo.user_name}'s posts`} />
+                <img src={personalData.profile_picture} alt="profile" />
+                <Title title={`${personalData.user_name}'s posts`} />
                 {user.id !== Number(id) && (
-                  <FollowStatusButton isFollowed={data.is_followed} setUpdate={setUpdate} id={id} update={update}/>
+                  <FollowStatusButton
+                    isFollowed={followStatus.is_followed}
+                    setUpdate={setUpdate}
+                    id={id}
+                    update={update}
+                  />
                 )}
               </TitleContainer>
-              {data.posts.map((p) => (
-                <Post key={p.post_share_id} post={p} renderPosts={renderPosts} />
-              ))}
+              <InfiniteScroll
+                pageStart={1}
+                loadMore={renderPosts} // load more pass the next page to the function as a parameter when the scroll hits the viewport
+                hasMore={hasMore}
+                loader={<ScrollLoading />}
+              >
+                {posts.map((p) => (
+                  <Post
+                    key={p.post_share_id}
+                    post={p}
+                    renderPosts={renderPosts}
+                  />
+                ))}
+              </InfiniteScroll>
             </MainContent>
             <HashtagTable />
           </>
